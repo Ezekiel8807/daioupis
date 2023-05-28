53414610e2d710f1dbc0720b51877a3526313fec
Ezekiel8807 <ayeoluwaseyi@gmail.com>
2023-05-28 21:21:41 +0100

    new changes

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-const logo = document.getElementById("logo");
-const navBar = document.getElementById("navbtn");
+const user = [
+  {
+    username: "Ezkiel8807",
+    firstname: "Ezekiel",
+    Lastname: "Ayebidun",
+    islogin: false,
+    password: "##Iyanu8807"
+  },
+  
+  {
+    username: "pelumi8807",
+    firstname: "pelumi",
+    Lastname: "Ayebidun",
+    islogin: false,
+    password: "##Pelumi8807"
+  }
+]
 
 //function run when page is fully loaded
-// document.addEventListener('DOMContentLoaded', function () {
-  // do something here ...
-//   alert("hello World")
-// });
+document.addEventListener('DOMContentLoaded', function () {
+
+  //do something here ...
+  if( window.location.href == "index.html"){
+    alert('Admin')
+  }
+});
 
 
 //function to open and close nav on mobile view
-navBar.addEventListener("click", ()=> {
+function show$hideNav() {
   let headerNav =  document.getElementById("headerNav");
 
   if (headerNav.style.display == "block"){
@@ -17,9 +35,9 @@ navBar.addEventListener("click", ()=> {
   }else {
     headerNav.style.display = "block";
   }
-});
+};
 
 //Back to homepage function
-logo.addEventListener("click", () => {
+function logo() {
   window.location.href = "./index.html";
-});
+};
--- backend/app.js
+++ backend/app.js
@@ -7,4 +7,14 @@ const port = 3000;
 const daioupis = express();
 
 
-daioupis.listen(port => console.log("Server started"))
+daioupis.get("/", (req, res) =>{
+
+});
+
+daioupis.get("/admin", (req, res) =>{
+    res.send("hello there!");
+});
+
+
+daioupis.listen(port);
+console.log(`server running at port ${port}`);
